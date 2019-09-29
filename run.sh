87ff59b31f8c527b0c824ccd01b11e8aef3ce5c9
thevivekcode <thevivekcode@gmail.com>
2019-09-29 19:46:05 +0530

    frist commit

---
+++ run.sh
@@ -0,0 +1,3 @@
+noweave -index -delay 2019MCS2574.nw > 2019MCS2574.tex
+texliveonfly 2019MCS2574.tex
+evince 2019MCS2574.pdf
---
+++ run2.sh
@@ -0,0 +1,1 @@
+notangle -R2019MCS2574-Module-complete 2019MCS2574.nw > 2019MCS2574.sml
